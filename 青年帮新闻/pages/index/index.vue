6c33cd7534a46fd7c3e67dc0aceb870684c8bebb
huangyijie <huang.yijie@iwhalecloud.com>
2023-01-31 20:32:40 +0800

    首页nav导航栏静态搭建

--- 青年帮新闻/pages/index/index.vue
+++ 青年帮新闻/pages/index/index.vue
@@ -1,6 +1,10 @@
 <template>
-	<view class="content">
-		<h1>首页</h1>
+	<view class="container">
+    <view class="nav">
+      <scroll-view scroll-x class="scroll">
+        <view class="item" v-for="item in 10">中国{{item}}</view>
+      </scroll-view>
+    </view>
 	</view>
 </template>
 
@@ -9,5 +13,27 @@
 </script>
 
 <style lang="less">
-	
+  .container {
+    .nav {
+      .scroll {
+        font-size: 40rpx;
+        display: flex;
+        height: 100rpx;
+        white-space: nowrap;
+        // 去除scroll-view带的滑动条（微信小程序中没有）
+        /deep/ ::-webkit-scrollbar {
+        	width: 4px !important;
+        	height: 1px !important;
+        	overflow: auto !important;
+        	background: transparent !important;
+        	-webkit-appearance: auto !important;
+        	display: block;
+        }
+        .item {
+          display: inline-block;
+          padding: 0 30rpx;
+        }
+      }
+    }
+  }
 </style>
