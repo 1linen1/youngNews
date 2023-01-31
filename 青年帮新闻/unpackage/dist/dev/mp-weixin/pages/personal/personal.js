b85b7be030db9a6eff02bec7c43738b730aba35e
huangyijie <huang.yijie@iwhalecloud.com>
2023-01-31 20:36:07 +0800

    导航栏nav静态搭建

---
+++ 青年帮新闻/unpackage/dist/dev/mp-weixin/pages/personal/personal.js
@@ -0,0 +1,8 @@
+"use strict";
+const common_vendor = require("../../common/vendor.js");
+const _sfc_main = {};
+function _sfc_render(_ctx, _cache) {
+  return {};
+}
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "D:/Resources/maven-workspace/app-space230122/\u9752\u5E74\u5E2E\u65B0\u95FB/pages/personal/personal.vue"]]);
+wx.createPage(MiniProgramPage);
